ab32816a318f744e8bd505f5e7ad38f386d14761
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-12 14:49:18 +0200

    feat: add alignment in the middle feature in portfolio section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,3 +23,31 @@ document.querySelectorAll(".nav__link").forEach((link) => {
 burgerBtn.addEventListener("click", openMenu);
 
 exitBtn.addEventListener("click", hideMenu);
+
+window.addEventListener("load", () => {
+  console.log("you");
+  const viewport = document.querySelector(".portfolio__viewport");
+  const slider = document.querySelector(".portfolio__slider");
+  const images = slider.querySelectorAll(".portfolio__img");
+
+  const img6 = images[5];
+  const img7 = images[6];
+
+  console.log("you");
+  if (viewport && img6 && img7) {
+    const middlePoint =
+      img6.offsetLeft +
+      img6.offsetWidth +
+      (img7.offsetLeft - (img6.offsetLeft + img6.offsetWidth)) / 2;
+
+    const scrollPosition = middlePoint - viewport.clientWidth / 2;
+
+    viewport.style.scrollSnapType = "none";
+
+    viewport.scrollTo({
+      left: scrollPosition,
+      behavior: "instant",
+    });
+    console.log("you");
+  }
+});
