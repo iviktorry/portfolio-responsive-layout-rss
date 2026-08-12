938bd1696dacdfe469a02610df7c1b3486092414
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-12 13:08:38 +0200

    feat: add menu window on mobile and tablet devices feature

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,25 @@
 import "../styles/main.css";
 
+const burgerBtn = document.querySelector(".header__burger-btn");
+const exitBtn = document.querySelector(".header__exit-btn");
+const navigationList = document.querySelector("#nav");
+
+const hideMenu = () => {
+  navigationList.classList.remove("is-open");
+  burgerBtn.classList.remove("hidden");
+  exitBtn.classList.add("hidden");
+};
+
+const openMenu = () => {
+  navigationList.classList.add("is-open");
+  burgerBtn.classList.add("hidden");
+  exitBtn.classList.remove("hidden");
+};
+
+document.querySelectorAll(".nav__link").forEach((link) => {
+  link.addEventListener("click", hideMenu);
+});
+
+burgerBtn.addEventListener("click", openMenu);
+
+exitBtn.addEventListener("click", hideMenu);
